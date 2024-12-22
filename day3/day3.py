ca02200d6ce994871a866162fa2688ae8ec3f1ba
ted <tlayher@gmail.com>
2024-12-22 10:12:18 -0500

    TLAY day3 part2

--- day3/day3.py
+++ day3/day3.py
@@ -10,18 +10,50 @@ def part1():
     lines = get_input('long.txt')
     total = 0
     for l in lines:
-        all = re.findall( "mul\(\d+,\d+\)", l)
+        all = re.findall( r"mul\((\d+),(\d+)\)", l)
         for a in all:
-            digits = re.findall("(\d+),(\d+)", a)
-            total += int(digits[0][0]) * int(digits[0][1])
+            d1,d2 = map(int, a)
+            total += d1 * d2
     print(total)
 
 
 
 def part2():
-    pass
+    lines = get_input('long.txt')
+    do_pattern = r'do\(\)'
+    dont_pattern = r'don\'t\(\)'
+    mul_pattern = r"mul\((\d+),(\d+)\)"
+    for l in lines:
+        mul = re.finditer( mul_pattern, l)
+        do = re.finditer(do_pattern, l)
+        dont = re.finditer(dont_pattern, l)
+
+    all = list(mul) + list(do) + list(dont)
+    regexes = sorted(all, key=lambda x: x.start())
+
+    total = 0
+    on = True
+    for r in regexes:    
+        if r.re.pattern == mul_pattern:
+            if on:
+                d1,d2 = map(int, r.groups())
+                total += d1 * d2
+                print(total)
+        elif r.re.pattern == do_pattern:
+            print("DO")
+            on = True
+        elif r.re.pattern == dont_pattern:
+            print("NOT")
+            on = False
+    print(total)
 
 
 if __name__ == "__main__":
     part1()
     part2()
+
+    #26362555 is too low
+    #18422941
+    #3719445
+    #69275878
+    #70478672
